9a40af3bc8b413d110f5994a77e759eeb6f6bbfb
Jegor Chomczuk <jegor.chomczuk@lhsystems.com>
2023-08-29 15:05:55 +0200

    init

---
+++ src/test/java/pl/training/trainticketbooking/TrainTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.training.trainticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
